2bf2e1daca931cdedc9b7839072d857bf93f15ab
Mike Griffin <mike.griffin@kindredgroup.com>
2022-09-01 14:18:57 +0200

    Initial Commit

---
+++ xcode cloud app1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  xcode cloud app1
+//
+//  Created by mike.griffin@kindredgroup.com on 01/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ xcode cloud app1/xcode_cloud_app1App.swift
@@ -0,0 +1,17 @@
+//
+//  xcode_cloud_app1App.swift
+//  xcode cloud app1
+//
+//  Created by mike.griffin@kindredgroup.com on 01/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct xcode_cloud_app1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
