43382e64d89659aa1842040aa6b132caaf6bd93b
Mike Griffin <mike.griffin@kindredgroup.com>
2023-06-07 15:30:16 +0200

    Change message again

--- xcode cloud app1/ContentView.swift
+++ xcode cloud app1/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world here I am!")
+        Text("Hello, world here I am on a white screen!")
             .padding()
     }
 }
