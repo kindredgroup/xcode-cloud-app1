c40242ce13007b4427a9a57b813e57128b90958a
Mike Griffin <mike.griffin@kindredgroup.com>
2023-06-08 12:50:53 +0200

    Use ls

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -1,9 +1,15 @@
 #!/bin/zsh
 
 echo PostBuild
+cd /Volumes/workspace/repository
+pwd
+ls -lah
 cd /Volumes/workspace/appstoreexport
+pwd
 ls -lah
 cd /Volumes/workspace/developmentexport
+pwd
 ls -lah
 cd /Volumes/workspace/appstoreexport
+pwd
 ls -lah
