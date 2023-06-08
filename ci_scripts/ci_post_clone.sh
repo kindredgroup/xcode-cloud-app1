a471b61eaf29d6cea84dafefa8d799e46f220296
Mike Griffin <mike.griffin@kindredgroup.com>
2023-06-08 11:56:51 +0200

    fix script

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,7 +1,8 @@
 #!/bin/zsh
 
+ls -lah
 cd /Volumes/workspace/
 ls -lah
 VARIABLES=$(env)
 echo $VARIABLES
-curl -H 'Content-Type: application/json' --location --request POST 'https://kindredgroup.webhook.office.com/webhookb2/24af7b04-0f90-4321-92c5-82aec1f7ee00@82ff090d-4ac0-439f-834a-0c3f3d5f33ce/IncomingWebhook/3d58293bf3a1400786da096f4e16e04c/8e740bb0-5c21-45e0-8327-fdbcc250530b' --d 'text="'$VARIABLES'"'
+echo curl -H 'Content-Type: application/json' -d '{"text": "$VARIABLES"}' https://kindredgroup.webhook.office.com/webhookb2/24af7b04-0f90-4321-92c5-82aec1f7ee00@82ff090d-4ac0-439f-834a-0c3f3d5f33ce/IncomingWebhook/3d58293bf3a1400786da096f4e16e04c/8e740bb0-5c21-45e0-8327-fdbcc250530b
